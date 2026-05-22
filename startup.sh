2f51d5cd23a7c5f710c5ce8196d66db90336b362
Shanereynolds1 <shanereynolds1@live.com>
2026-05-22 18:10:11 -0500

    fix: shallow clone in startup.sh to reduce container start time

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ set -e
 GITHUB_TOKEN=$(echo "$GITHUB_TOKEN" | tr -d '\r\n ')
 
 # Clone latest repo so scripts and manifest are always fresh
-git clone "https://x-access-token:${GITHUB_TOKEN}@github.com/shanegk12/Genesis-AIOS.git" /repo
+git clone --depth=1 --single-branch --branch=main "https://x-access-token:${GITHUB_TOKEN}@github.com/shanegk12/Genesis-AIOS.git" /repo
 cd /repo
 git config user.email "pipeline@genesis-aios.iam.gserviceaccount.com"
 git config user.name "GK12 Pipeline"
